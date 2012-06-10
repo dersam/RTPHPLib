1c4610e4d6ffe022d2ab9cdedcab30c27c2bb72c
Sam Schmidt <samuel@dersam.net>
2012-06-09 22:03:00 -0400

    added link to demos site to example

--- example.php
+++ example.php
@@ -1,66 +1,68 @@
 <?php
 require_once 'RequestTracker.php';
 
-$url = "http://rt.example.com";
-$user = "user";
-$pass = "password";
+$url = "http://rt.easter-eggs.org/demos/stable";
+$user = "admin";
+$pass = "admin";
 
 $rt = new RequestTracker($url, $user, $pass);
 
 //Get the properties of an existing ticket, given the ID
-/*
-$response = $rt->getTicketProperties(1);
-*/
+   /*
+$response = $rt->getTicketProperties(233);
+print_r($response);
+        */
 //Create a new ticket- see http://requesttracker.wikia.com/wiki/REST#Ticket_Create for all fields
-/*
+        /*
 $content = array(
-    'Queue'=>'General',
+    'Queue'=>'fast_lane',
     'Requestor'=>'test@example.com',
     'Subject'=>'Lorem Ipsum',
     'Text'=>'dolor sit amet'
 );
 $response = $rt->createTicket($content);
 print_r($response);
- */
+     */
 
 //Edit a ticket
-/*
+       /*
 $content = array(
-    'Priority'=>5
+    'Priority'=>3
 );
-$response = $rt->editTicket(3, $content);
+$response = $rt->editTicket(233, $content);
 print_r($response);
-*/
+       */
 
 //Reply to a ticket
-/*
+  /*
 $content = array(
     'Text'=>'This is a test reply.'
 );
-$response = $rt->doTicketReply(3, $content);
+$response = $rt->doTicketReply(233, $content);
 print_r($response);
-*/
 
+       */
 //Comment on a ticket
-/*
+           /*
 $content = array(
     'Text'=>'This is a test comment.'
 );
-$response = $rt->doTicketComment(3, $content);
+$response = $rt->doTicketComment(233, $content);
 print_r($response);
- */
+      */
 
 //Ticket Links
-/*
+         /*
 $content = array(
     'DependsOn'=>1
 );
 
-$response = $rt->editTicketLinks(3, $content);
-print_r($response);
-$response = $rt->getTicketLinks(3);
-print_r($response);
-*/
+$response = $rt->editTicketLinks(233, $content);
+print_r($response);    */
+/*
+$response = $rt->getTicketLinks(233);
+print_r($response);  */
+
 
 //Attachment management
 /*
@@ -73,17 +75,17 @@ print_r($response);
  */
 
 //Ticket History
-/*
-$response = $rt->getTicketHistory(3);
+        /*
+$response = $rt->getTicketHistory(233);
 print_r($response);
-$response = $rt->getTicketHistoryNode(3, 44);
+$response = $rt->getTicketHistoryNode(233,3702);
 print_r($response);
- */
+      */
 
 //Search demonstration
 //Unowned tickets ordered by Created date descending
-/*
-$response = $rt->search("Owner='Nobody'",'-Created');
-print_r($response);
-*/
+            /*
+$response = $rt->search("Owner='Nobody'",'-Created', 's');
+print_r($response);   */
+
 ?>
