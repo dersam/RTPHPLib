b2f1b419e05e8ef57478c0bb6afce6611f87d181
Sam Schmidt <samuel@dersam.net>
2015-04-13 19:43:15 -0400

    Added easter-eggs RT sample credentials.

--- tests/RequestTrackerTest.php
+++ tests/RequestTrackerTest.php
@@ -1,5 +1,7 @@
 <?php
 
 class RequestTrackerTest extends PHPUnit_Framework_TestCase{
-
+    protected $host = 'http://rt.easter-eggs.org/demos/4.2/';
+    protected $user = 'admin';
+    protected $password = 'admin';
 }
