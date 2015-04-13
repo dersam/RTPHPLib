0bc81b2d46765b0ac6f41d74954034774604dd27
Sam Schmidt <samuel@dersam.net>
2015-04-13 19:35:52 -0400

    Added travis test harness.

---
+++ tests/RequestTrackerTest.php
@@ -0,0 +1,5 @@
+<?php
+
+class RequestTrackerTest extends PHPUnit_Framework_TestCase{
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+date_default_timezone_set('UTC');
+
+function loader($class)
+{
+    $file = $class . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+}
+spl_autoload_register('loader');
