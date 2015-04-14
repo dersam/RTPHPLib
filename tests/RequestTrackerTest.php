128c1df2e86eee1a2e8b596e9414faa5eafb7240
Sam Schmidt <samuel@dersam.net>
2015-04-13 20:15:33 -0400

    create ticket test

--- tests/RequestTrackerTest.php
+++ tests/RequestTrackerTest.php
@@ -4,4 +4,26 @@ class RequestTrackerTest extends PHPUnit_Framework_TestCase{
     protected $host = 'http://rt.easter-eggs.org/demos/4.2/';
     protected $user = 'admin';
     protected $password = 'admin';
+    /** @var  RequestTracker */
+    protected $client;
+
+    public function setUp(){
+       $this->client = new RequestTracker(
+           $this->host,
+           $this->user,
+           $this->password
+       );
+    }
+
+    public function tearDown(){
+
+    }
+
+    public function testCreateTicket(){
+        $response = $this->client->createTicket(array(
+            'Queue'=>'General',
+            'Text'=>'This is a test ticket.'
+        ));
+        print_r($response);
+    }
 }
