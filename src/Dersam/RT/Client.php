f6054061edc356fd7aac26a0ec17c8f5b5c73e61
Sam Schmidt <samuel@dersam.net>
2015-04-14 22:25:26 -0400

    SSL verification toggle.

--- src/Dersam/RT/Client.php
+++ src/Dersam/RT/Client.php
@@ -3,5 +3,21 @@ namespace Dersam\RT;
 
 
 class Client {
+    protected $verifySsl = true;
 
+    /**
+     * @return boolean
+     */
+    public function isVerifyingSsl()
+    {
+        return $this->verifySsl;
+    }
+
+    /**
+     * @param boolean $verifySsl
+     */
+    public function setVerifySsl($verifySsl)
+    {
+        $this->verifySsl = $verifySsl;
+    }
 }
