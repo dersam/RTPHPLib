c010075729fd2898f0566b100897ec9db1e4f0ac
Sam Schmidt <samuel@dersam.net>
2015-04-15 20:59:40 -0400

    Base request stub.

--- src/Dersam/RT/Clients/V1Client.php
+++ src/Dersam/RT/Clients/V1Client.php
@@ -7,8 +7,16 @@
 namespace Dersam\RT\Clients;
 
 use Dersam\RT\Client;
+use Dersam\RT\Responses\v1\Base;
 
 class V1Client extends Client
 {
-
+    /**
+     * The default API response from the base url
+     * @return \Dersam\RT\Responses\v1\Base
+     */
+    public function base()
+    {
+        $request = new \Dersam\RT\Requests\v1\Base();
+    }
 }
---
+++ tests/V1ClientTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * RTPHPLib
+ * User: Sam
+ * Date: 4/15/2015
+ */
+
+namespace Dersam\RT;
+
+class V1ClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBaseRequest()
+    {
+
+    }
+}
