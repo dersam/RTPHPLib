658f0381dd1239abda65460aed34011dc9fc6b9b
Sam Schmidt <samuel@dersam.net>
2015-04-15 20:37:31 -0400

    Imported response parser from v1

--- src/Dersam/RT/Responses/V1Response.php
+++ src/Dersam/RT/Responses/V1Response.php
@@ -12,6 +12,32 @@ class V1Response extends Response
 {
     public function parse($code, $response)
     {
-        // TODO: Implement parse() method.
+        $responseArray = array();
+        $response = explode(chr(10), trim($response['body']));
+        array_shift($response); //skip RT status response
+        array_shift($response); //skip blank line
+        $lastkey = null;
+        foreach ($response as $line) {
+            //RT will always preface a multiline with at least one space
+            if (substr($line, 0, 1)==' ') {
+                $responseArray[$lastkey] .= "\n".trim($line);
+                continue;
+            }
+            $parts = explode(':', $line);
+            $key = array_shift($parts);
+            $value = implode(':', $parts);
+
+            // push comments to end of array, ignore blank keys
+            if ($key !== '') {
+                if (strpos($key, '#')===0) {
+                    $responseArray[] = $key;
+                } else {
+                    $responseArray[$key] = trim($value);
+                }
+
+            }
+        }
+
+        return $responseArray;
     }
 }
