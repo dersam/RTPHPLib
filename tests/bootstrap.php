253f9e0378256fff9fc87599cefbc15120aac996
Sam Schmidt <samuel@dersam.net>
2015-04-15 20:12:27 -0400

    Cleanup of test bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,10 @@
 <?php
 date_default_timezone_set('UTC');
 
-function loader($class)
-{
+
+spl_autoload_register(function ($class) {
     $file = $class . '.php';
     if (file_exists($file)) {
         require $file;
     }
-}
-spl_autoload_register('loader');
+});
