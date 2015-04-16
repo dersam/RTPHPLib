9a38a8c2573a58882ab94f1db37ad694a0610c5f
Sam Schmidt <samuel@dersam.net>
2015-04-15 21:08:16 -0400

    Function stubs for v1client.

--- src/Dersam/RT/Clients/V1Client.php
+++ src/Dersam/RT/Clients/V1Client.php
@@ -15,8 +15,93 @@ class V1Client extends Client
      * The default API response from the base url
      * @return \Dersam\RT\Responses\v1\Base
      */
-    public function base()
+    public function getBaseRequest()
     {
         $request = new \Dersam\RT\Requests\v1\Base();
+    }
+
+    public function doCreateTicket()
+    {
+
+    }
+
+    public function doEditTicket()
+    {
+
+    }
+
+    public function doReplyTicket()
+    {
+
+    }
+
+    public function doCommentTicket()
+    {
+
+    }
+
+    public function getTicketProperties()
+    {
+
+    }
+
+    public function getTicketLinks()
+    {
+
+    }
+
+    public function doEditTicketLinks()
+    {
+
+    }
+
+    public function getTicketAttachments()
+    {
+
+    }
+
+    public function getAttachmentMetadata()
+    {
+
+    }
+
+    public function getAttachmentContent()
+    {
+
+    }
+
+    public function getTicketHistory()
+    {
+
+    }
+
+    public function getTicketHistoryNode()
+    {
+
+    }
+
+    public function searchI()
+    {
+
+    }
+
+    public function searchS()
+    {
+
+    }
+
+    public function searchL()
+    {
+
+    }
+
+    public function getUserProperties()
+    {
+
+    }
+
+    public function getQueueProperties()
+    {
+
     }
 }
