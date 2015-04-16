d6bc76a0e42a4b0fdbdf14d41ad8eeb8e3d2b1f8
Sam Schmidt <samuel@dersam.net>
2015-04-15 21:05:29 -0400

    Test stubs for v1 client.

--- tests/V1ClientTest.php
+++ tests/V1ClientTest.php
@@ -9,7 +9,92 @@ namespace Dersam\RT;
 
 class V1ClientTest extends \PHPUnit_Framework_TestCase
 {
-    public function testBaseRequest()
+    public function testGetBaseRequest()
+    {
+
+    }
+
+    public function testDoCreateTicket()
+    {
+
+    }
+
+    public function testDoEditTicket()
+    {
+
+    }
+
+    public function testDoReplyTicket()
+    {
+
+    }
+
+    public function testDoCommentTicket()
+    {
+
+    }
+
+    public function testGetTicketProperties()
+    {
+
+    }
+
+    public function testGetTicketLinks()
+    {
+
+    }
+
+    public function testDoEditTicketLinks()
+    {
+
+    }
+
+    public function testGetTicketAttachments()
+    {
+
+    }
+
+    public function testGetAttachmentMetadata()
+    {
+
+    }
+
+    public function testGetAttachmentContent()
+    {
+
+    }
+
+    public function testGetTicketHistory()
+    {
+
+    }
+
+    public function testGetTicketHistoryNode()
+    {
+
+    }
+
+    public function testSearchI()
+    {
+
+    }
+
+    public function testSearchS()
+    {
+
+    }
+
+    public function testSearchL()
+    {
+
+    }
+
+    public function testGetUserProperties()
+    {
+
+    }
+
+    public function testGetQueueProperties()
     {
 
     }
