6e1b62cd7fca57369f64f0ba8af6f966c05c300e
Sam Schmidt <samuel@dersam.net>
2015-04-16 23:10:51 -0400

    Stub request and responses.

---
+++ src/Dersam/RT/Requests/v1/GetAttachmentContent.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * RTPHPLib
+ * User: Sam
+ * Date: 4/16/2015
+ */
+namespace Dersam\RT\Requests\v1;
+
+use Dersam\RT\Requests\V1Request;
+use Dersam\RT\Response;
+
+class GetAttachmentContent extends V1Request
+{
+
+    /**
+     * @return Response
+     */
+    public function makeResponseInstance()
+    {
+        // TODO: Implement makeResponseInstance() method.
+    }
+}
